af4335aaa3455127748676a5f513f78b87a4469b
AndresDamian0412 <andres.mc.damian@gmail.com>
2021-11-17 02:05:51 -0600

    Funciones en el JS

--- javascript/login.js
+++ javascript/login.js
@@ -1,4 +1,8 @@
 function esconderForm(){
-    document.getElementById("formEmploye").style.display = 'none';
-    document.getElementById("formStudent").style.display = 'none';
+    document.getElementById("formEmploye").style.visibility = 'collapse';
+    document.getElementById("formStudent").style.visibility = 'collapse';
+}
+
+function cambiarForm(){
+    document.getElementById(this.id).style.visibility = "visible";
 }
