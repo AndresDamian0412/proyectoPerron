75fdacfa35023c6f6077714c98a691e42a85d6d2
AndresDamian0412 <andres.mc.damian@gmail.com>
2021-11-22 04:16:53 -0600

    Estilos al Login

--- NewVersion/JS/login.js
+++ NewVersion/JS/login.js
@@ -1,3 +1,12 @@
+var cambio=1;
 function mostrarForm() {
-    document.getElementById("formRegister").style.visibility="visible";
+    if(cambio==1){
+        document.getElementById("formRegister").style.visibility="visible";
+        cambio+=1;
+        console.log(cambio);
+    }else if(cambio==2){
+        document.getElementById("formRegister").style.visibility="hidden";
+        cambio-=1;
+        console.log(cambio);
+    }
 }
