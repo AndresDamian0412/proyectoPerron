2291bd68d7fbefae32608e7620db759c01ddc27b
AndresDamian0412 <andres.mc.damian@gmail.com>
2021-12-02 17:25:06 -0600

    Listos Log In y Sing Up!

--- NewVersion/PHP/login.php
+++ NewVersion/PHP/login.php
@@ -1,24 +1,30 @@
 <?php
-include_once "conexion.php";
+  include_once "conexion.php";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {  
-  $user = $_POST["getUser"];
-  $pwd = $_POST["getPass"];
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {  
+    $user = $_POST["getUser"];
+    $pwd = $_POST["getPass"];
 
-  $BD = new conexionBD();
-  $sql = "SELECT * FROM usuarios WHERE NomUsuario= '".$user."' and Contra = '".$pwd."'";
-  $stmt = $BD->ejecutar($sql);
-  $resultado= $stmt->fetch(PDO::FETCH_ASSOC);
-  if($resultado){
-    if($resultado["TipoUser"]=="User"){
-      header("Location: ../HTML/user-mainview.html");
-    }else if($resultado["TipoUser"]=="Admin"){
-      header("Location: ../HTML/admin-mainview.html");
+    $BD = new conexionBD();
+    $sql = "SELECT * FROM usuarios WHERE NomUsuario= '".$user."' and Contra = '".$pwd."'";
+    $stmt = $BD->ejecutar($sql);
+    $resultado= $stmt->fetch(PDO::FETCH_ASSOC);
+    if($resultado){
+      if($resultado["TipoUser"]=="User"){
+        header("Location: ../HTML/user-mainview.html");
+      }else if($resultado["TipoUser"]=="Admin"){
+        header("Location: ../HTML/admin-mainview.html");
+      }
+    }else{
+      //echo "<script>alert('Verifique sus credenciales, acceso denegado.')</script>";
+      //header("Location: ../HTML/login.html");
+      //echo "<script>alert('Verifique sus credenciales, acceso denegado.')</script>";
+      echo'<script type="text/javascript">
+      window.location.href="../HTML/login.html";
+      alert("Acceso denegado, verifique sus datos.");
+      </script>';
     }
-  }else{
-    header("Location: ../HTML/login.html");
+    
+    $stmt->closeCursor();
   }
-  
-  $stmt->closeCursor();
-}
 ?>
---
+++ NewVersion/PHP/singup.php
@@ -0,0 +1,37 @@
+<?php
+    include_once "conexion.php";
+
+    if ($_SERVER["REQUEST_METHOD"]=="POST") {
+        $nomUser = $_POST["getUser"];
+        $email = $_POST["getMail"];
+        $pwd = $_POST["getPass"];
+        $tel = $_POST["getPhone"];
+        $tipoUser = "User";
+        
+        $BD = new conexionBD();
+        $sql1 = "SELECT NomUsuario FROM usuarios WHERE NomUsuario= '".$nomUser."'";
+        $stmt1 = $BD->ejecutar($sql1);
+        $resultado1= $stmt1->fetch(PDO::FETCH_ASSOC);
+        
+        if ($nomUser == $resultado1["NomUsuario"]) {
+            echo'<script type="text/javascript">
+            window.location.href="../HTML/login.html";
+            alert("Lo sentimos, el nombre de usuario '.$nomUser.' ya se encuentra registrado...");
+            </script>';
+        }else{
+            $sql2 = "INSERT INTO usuarios VALUES('".$nomUser."','".$email."','".$pwd."','".$tel."','".$tipoUser."')";
+            $stmt2 = $BD->ejecutar($sql2);
+            if ($stmt2->rowCount()==1) {
+                echo'<script type="text/javascript">
+                window.location.href="../HTML/login.html";
+                alert("Usuario registrado exitosamente!!");
+                </script>';
+            }else{
+                echo'<script type="text/javascript">
+                window.location.href="../HTML/login.html";
+                alert("Error inesperado, vuelva a intentarlo...");
+                </script>';
+            }
+        }
+    }
+?>
