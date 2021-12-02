a51978d0b2699349e8cae6b3dcdea55a4e104cdf
AndresDamian0412 <andres.mc.damian@gmail.com>
2021-12-02 08:38:47 -0600

    Login listo

--- NewVersion/PHP/login.php
+++ NewVersion/PHP/login.php
@@ -1,27 +1,24 @@
 <?php
 include_once "conexion.php";
 
-if ($_SERVER["REQUEST_METHOD"] == "GET") {  
-  //$user = $_GET["nombre"];
-  //$pwd = $_GET["clave"];
-  $user ="Andres";
-  $pwd ="123";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {  
+  $user = $_POST["getUser"];
+  $pwd = $_POST["getPass"];
 
   $BD = new conexionBD();
   $sql = "SELECT * FROM usuarios WHERE NomUsuario= '".$user."' and Contra = '".$pwd."'";
   $stmt = $BD->ejecutar($sql);
-
-  $array;
-  $i=0;
-
-  while($resultado= $stmt->fetch(PDO::FETCH_ASSOC)){
-    $array[$i] = $resultado;
-    $i++;
+  $resultado= $stmt->fetch(PDO::FETCH_ASSOC);
+  if($resultado){
+    if($resultado["TipoUser"]=="User"){
+      header("Location: ../HTML/user-mainview.html");
+    }else if($resultado["TipoUser"]=="Admin"){
+      header("Location: ../HTML/admin-mainview.html");
+    }
+  }else{
+    header("Location: ../HTML/login.html");
   }
-
-  $json_datos = json_encode($array);
-  echo $json_datos;
+  
   $stmt->closeCursor();
-
 }
 ?>
